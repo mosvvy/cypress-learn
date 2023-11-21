752c2a71c922c65c4f12d0c237100cb90847f726
mosvvy <moskalenkovvy@gmail.com>
2023-11-21 17:09:56 +0200

    Add test for log in

--- cypress/e2e/E2E-testing-baseURL-site.cy.js
+++ cypress/e2e/E2E-testing-baseURL-site.cy.js
@@ -1,6 +1,21 @@
 describe('The Home Page', () => {
     it('successfully loads', () => {
       cy.visit('/')
+      /**
+       * admin:
+       *  demo_admin
+       *  demo_admin123
+       * customer:
+       *  roni_cost@example.com
+       *  roni_cost3@example.com
+       */
       cy.contains('Sign In').click()
+
+      cy.get('[id=email]').type('roni_cost@example.com')
+      cy.get('.input-text[id=pass]').first().type('roni_cost3@example.com')
+      cy.get('.primary[id=send2]').first().click()
+
+      cy.wait(1000)
+      cy.get('span.logged-in').first().should('have.text', 'Welcome, Veronica Costello!')
     })
   })
